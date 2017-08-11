6707d81a96c6d2bf186a5cdc8eebd802ea921637
nicklupashko <n.r.lupashko@gmail.com>
2017-08-11 17:19:13 +0300

    [zero] `flatMap`

--- src/main/scala/zero.scala
+++ src/main/scala/zero.scala
@@ -1,3 +1,9 @@
 object zero extends App {
 
+  def flatMap[A, B](fa: () => A)(f: A => () => B): () => B =
+    () => f(fa())()
+
+  def flatMap[T1, R1, R2](fa: T1 => R1)(f: R1 => T1 => R2): T1 => R2 =
+    t => f(fa(t))(t)
+
 }
