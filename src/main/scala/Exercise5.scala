01636b69c271121c334ab715407c87d4eab25ccb
nicklupashko <n.r.lupashko@gmail.com>
2017-08-14 12:19:55 +0300

    [Exercise5] Adding All The Things Part 3

--- src/main/scala/Exercise5.scala
+++ src/main/scala/Exercise5.scala
@@ -11,4 +11,15 @@ object Exercise5 extends App {
   println(add(List(1, 2, 3)))
   println(add(List(Some(1), None, Some(2), None)))
 
+  case class Order(totalCost: Double, quantity: Double)
+
+  implicit val monoid: Monoid[Order] = new Monoid[Order] {
+    def empty: Order = Order(0, 0)
+
+    def combine(x: Order, y: Order): Order =
+      Order(x.totalCost + y.totalCost, x.quantity + y.quantity)
+  }
+
+  println(add(List(Order(1, 1), Order(2, 2))))
+
 }
