5fb4e5d2841ff4ea84faa750ea144f93227183b4
nicklupashko <n.r.lupashko@gmail.com>
2017-08-14 13:08:40 +0300

    [Exercise6] Branching out with Functors

--- src/main/scala/Exercise6.scala
+++ src/main/scala/Exercise6.scala
@@ -7,5 +7,25 @@ object Exercise6 extends App {
 
   final case class Leaf[A](value: A) extends Tree[A]
 
+  import cats.Functor
+  import cats.syntax.functor._
+
+  implicit val treeFunctor = new Functor[Tree] {
+    def map[A, B](tree: Tree[A])(func: (A) => B): Tree[B] =
+      tree match {
+        case Branch(left, right) =>
+          Branch(map(left)(func), map(right)(func))
+        case Leaf(value) =>
+          Leaf(func(value))
+      }
+  }
+
+  def branch[A](left: Tree[A], right: Tree[A]): Tree[A] =
+    Branch(left, right)
+
+  def leaf[A](value: A): Tree[A] =
+    Leaf(value)
+
+  println(branch(leaf(10), leaf(20)).map(_ * 2))
 
 }
