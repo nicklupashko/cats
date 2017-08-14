c0ae0d6502e929a28d802e8e4e8a5f5e985b1edf
nicklupashko <n.r.lupashko@gmail.com>
2017-08-14 12:16:54 +0300

    [Exercise5] Adding All The Things Part 2

--- src/main/scala/Exercise5.scala
+++ src/main/scala/Exercise5.scala
@@ -3,10 +3,12 @@ object Exercise5 extends App {
   import cats.Monoid
   import cats.syntax.semigroup._
   import cats.instances.int._
+  import cats.instances.option._
 
-  def add(items: List[Int]): Int =
-    items.foldLeft(Monoid[Int].empty)(_ |+| _)
+  def add[A: Monoid](items: List[A]): A =
+    items.foldLeft(Monoid[A].empty)(_ |+| _)
 
   println(add(List(1, 2, 3)))
+  println(add(List(Some(1), None, Some(2), None)))
 
 }
