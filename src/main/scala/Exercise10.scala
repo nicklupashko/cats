49191bd910d222cff0d348dd9b5b70a20f6b24f5
nicklupashko <n.r.lupashko@gmail.com>
2017-08-14 17:39:08 +0300

    [Exercise10] Monadic Secret Identities

---
+++ src/main/scala/Exercise10.scala
@@ -0,0 +1,13 @@
+object Exercise10 extends App {
+
+  import cats.Id
+
+  def pure[A](value: A): Id[A] = value
+
+  def map[A, B](initial: Id[A])(func: A => B): Id[B] =
+    func(initial)
+
+  def flatMap[A, B](initial: Id[A])(func: A => Id[B]): Id[B] =
+    func(initial)
+
+}
