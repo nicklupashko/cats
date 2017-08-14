34e5160a8f16e7b87377b80d13f962f935bb1736
nicklupashko <n.r.lupashko@gmail.com>
2017-08-14 15:53:17 +0300

    [Exercise8] Transformative Thinking with Imap

---
+++ src/main/scala/Exercise8.scala
@@ -0,0 +1,42 @@
+object Exercise8 extends App {
+
+  trait Codec[A] {
+    def encode(value: A): String
+
+    def decode(value: String): Option[A]
+
+    def imap[B](dec: A => B, enc: B => A): Codec[B] = {
+      val self = this
+      new Codec[B] {
+        def encode(value: B): String =
+          self.encode(enc(value))
+
+        def decode(value: String): Option[B] =
+          self.decode(value).map(dec)
+      }
+    }
+  }
+
+  def encode[A](value: A)(implicit c: Codec[A]): String =
+    c.encode(value)
+
+  def decode[A](value: String)(implicit c: Codec[A]): Option[A] =
+    c.decode(value)
+
+  implicit val intCodec = new Codec[Int] {
+    def encode(value: Int): String =
+      value.toString
+
+    def decode(value: String): Option[Int] =
+      scala.util.Try(value.toInt).toOption
+  }
+
+  case class Box[A](value: A)
+
+  implicit def boxCodec[A](implicit c: Codec[A]): Codec[Box[A]] =
+    c.imap[Box[A]](Box(_), _.value)
+
+  println(encode(Box(123)))
+  println(decode[Box[Int]]("123"))
+
+}
