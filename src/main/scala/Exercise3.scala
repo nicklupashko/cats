6780b350497069c799eec42999b4754f60b0b856
nicklupashko <n.r.lupashko@gmail.com>
2017-08-14 11:22:10 +0300

    [Exercise3] The Truth About Monoids

--- src/main/scala/Exercise3.scala
+++ src/main/scala/Exercise3.scala
@@ -1,3 +1,42 @@
 object Exercise3 extends App {
 
+  trait Semigroup[A] {
+    def combine(x: A, y: A): A
+  }
+
+  trait Monoid[A] extends Semigroup[A] {
+    def empty: A
+  }
+
+  object Monoid {
+    def apply[A](implicit monoid: Monoid[A]): Monoid[A] = monoid
+  }
+
+  implicit val booleanAndMonoid: Monoid[Boolean] =
+    new Monoid[Boolean] {
+      def combine(x: Boolean, y: Boolean): Boolean = x && y
+      def empty: Boolean = true
+    }
+
+  implicit val booleanOrMonoid: Monoid[Boolean] =
+    new Monoid[Boolean] {
+      def combine(x: Boolean, y: Boolean): Boolean = x || y
+      def empty: Boolean = false
+    }
+
+  implicit val booleanEitherMonoid: Monoid[Boolean] =
+    new Monoid[Boolean] {
+      def combine(x: Boolean, y: Boolean): Boolean =
+        (x && !y) || (!x && y)
+
+      def empty: Boolean = false
+    }
+
+  implicit val booleanXnorMonoid: Monoid[Boolean] =
+    new Monoid[Boolean] {
+      def combine(x: Boolean, y: Boolean): Boolean =
+        (!x || y) && (x || !y)
+
+      def empty: Boolean = true
+    }
 }
